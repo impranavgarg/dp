7afc682f50b92335a84992184bf2d42e1357b29b
impranavgarg <impranavgarg@gmail.com>
2023-07-17 00:19:02 +0530

    more approches of knapsack with tabulation

--- knapSack_01.java
+++ knapSack_01.java
@@ -1,65 +1,93 @@
-class knapSack_01{
+class knapSack_01 {
 
     // O(2^W)
-    private static int knapSack(int val[], int wt[] , int W, int n){
+    private static int knapSack(int val[], int wt[], int W, int n) {
 
-        if(W==0 || n==0){
-            return 0 ;
+        if (W == 0 || n == 0) {
+            return 0;
         }
-        if(wt[n-1] <=W) {
+        if (wt[n - 1] <= W) {
             // include
-            int ans1 = val[n-1] + knapSack(val,wt,W-wt[n-1],n-1) ;
-            //exclude
-            int ans2 = knapSack(val,wt,W,n-1);
-            return Math.max(ans2,ans1);
-        }else{
-            return knapSack(val,wt,W,n-1);
+            int ans1 = val[n - 1] + knapSack(val, wt, W - wt[n - 1], n - 1);
+            // exclude
+            int ans2 = knapSack(val, wt, W, n - 1);
+            return Math.max(ans2, ans1);
+        } else {
+            return knapSack(val, wt, W, n - 1);
         }
     }
 
     // O(n*W) ---> usning Memoization
-    private static int knapSackMemoization(int val[], int wt[] , int W, int n, int[][] dp){
+    private static int knapSackMemoization(int val[], int wt[], int W, int n, int[][] dp) {
 
-        if(W==0 || n==0){
-            return 0 ;
+        if (W == 0 || n == 0) {
+            return 0;
         }
-        if(dp[n][W]!=-1){
+        if (dp[n][W] != -1) {
             return dp[n][W];
         }
 
-        if(wt[n-1] <=W) {
+        if (wt[n - 1] <= W) {
             // include
-            int ans1 = val[n-1] + knapSackMemoization(val,wt,W-wt[n-1],n-1,dp) ;
-            //exclude
-            int ans2 = knapSackMemoization(val,wt,W,n-1,dp);
-            dp[n][W] =  Math.max(ans2,ans1);
+            int ans1 = val[n - 1] + knapSackMemoization(val, wt, W - wt[n - 1], n - 1, dp);
+            // exclude
+            int ans2 = knapSackMemoization(val, wt, W, n - 1, dp);
+            dp[n][W] = Math.max(ans2, ans1);
             return dp[n][W];
-        }else{
-            dp[n][W] = knapSackMemoization(val,wt,W,n-1, dp);
+        } else {
+            dp[n][W] = knapSackMemoization(val, wt, W, n - 1, dp);
             return dp[n][W];
         }
     }
 
-    // using Tabulation 
-    
-    public static void main(String[] args) {
-        int [] val = {15,14,10,45,30};
-        int wt[] ={2,5,1,3,4};
-        int W = 7;
-        int[][] dp = new int[val.length+1][W+1];
+    // using Tabulation --> O(n*w)
+    private static int knapSackTabulation(int[] val, int[] wt, int w) {
+        int[][] dp = new int[val.length + 1][w + 1];
+        int n =val.length;
         for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[0].length; j++) {
-                dp[i][j]=-1;
+            dp[i][0]=0;
+
+        }
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j]=0;
+
+        }
+        for(int i =1;i<n+1;i++){
+            for (int j = 1; j < w+1; j++) {
+                int value = val[i-1]; // ith item value
+                int weight = wt[i-1]; // ith item weight
+                if(weight<=j){ // valid
+                    int incProfit = value+ dp[i-1][j-weight];
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                }else{
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = excProfit;
+                }
                 
             }
-            
+        }
+        return dp[n][w];
+
+    }
+
+    public static void main(String[] args) {
+        int[] val = { 15, 14, 10, 45, 30 };
+        int wt[] = { 2, 5, 1, 3, 4 };
+        int W = 7;
+        int[][] dp = new int[val.length + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+
+            }
+
         }
 
+        System.out.println(knapSackTabulation(val, wt, W));
 
+        System.out.println(knapSack(val, wt, W, val.length));
+        System.out.println(knapSackMemoization(val, wt, W, val.length, dp));
 
-
-        System.out.println(knapSack(val,wt,W,val.length));
-        System.out.println(knapSackMemoization(val,wt,W,val.length, dp));
-        
     }
 }
---
+++ targetSumSubset.java
@@ -0,0 +1,19 @@
+public class targetSumSubset {
+
+    public static void main(String[] args) {
+        int [] numbers  = {4,2,7,1,3};
+        int targetSum = 10;
+
+
+        System.out.println(targetSumFind(numbers, targetSum));
+        
+    }
+
+    private static boolean targetSumFind(int[] numbers, int targetSum) {
+
+        return false;
+
+
+
+    }
+}
